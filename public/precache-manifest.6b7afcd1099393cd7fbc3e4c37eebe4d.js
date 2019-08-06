eca68a378368a70b4a2be05b02ae4d4efa7750c5
Dmitry Dergousov <dodergousov@tsum.ru>
2019-08-07 00:32:43 +0300

    Update !

---
+++ public/precache-manifest.6b7afcd1099393cd7fbc3e4c37eebe4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fb7977abadc54b7ff4180fb35e45fc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dcfc7ccb0e2cf1c5259",
+    "url": "/static/css/main.2a94895b.chunk.css"
+  },
+  {
+    "revision": "0b23b3c482d0dc873f94",
+    "url": "/static/js/2.82999f11.chunk.js"
+  },
+  {
+    "revision": "3dcfc7ccb0e2cf1c5259",
+    "url": "/static/js/main.5582cbe8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "48ed9486a849ca2dbb86698445877bb8",
+    "url": "/static/media/AMO.48ed9486.png"
+  }
+]);
